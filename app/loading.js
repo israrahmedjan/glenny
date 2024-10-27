502e6f544e7960be7086b6114e582af598043e86
israrahmedjan <israrahmedjan@gmail.com>
2024-10-26 20:21:55 -0700

    'loader'

---
+++ app/loading.js
@@ -0,0 +1,9 @@
+// src/app/loading.js
+
+const { default: LoadingSpinner } = require("./_utils/LoadingSpinner");
+
+const Loading = () => {
+  return <LoadingSpinner />;
+};
+
+export default Loading;
