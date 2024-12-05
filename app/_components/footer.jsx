2894d28e184abd9b091145c0fe91629051e4fcac
israrahmedjan <123giIsrar456>
2024-12-05 18:56:17 +0500

    Number of files deleted

--- app/_components/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function FooterPage() {
-  return (
-    <div>This is footer page</div>
-  )
-}
-
-export default Page
--- app/_components/header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function HeaderPage() {
-  return (
-    <div>This is Header Page</div>
-  )
-}
-
-export default header
