27cfeaee4e2b2cd859de919fe0f368b4fddd73fd
israrahmedjan <123giIsrar456>
2024-12-06 18:01:20 +0500

    Files changes updated...

--- app/loading.js
+++
@@ -1,9 +0,0 @@
-// src/app/loading.js
-
-const { default: LoadingSpinner } = require("./_utils/LoadingSpinner");
-
-const Loading = () => {
-  return <LoadingSpinner />;
-};
-
-export default Loading;
