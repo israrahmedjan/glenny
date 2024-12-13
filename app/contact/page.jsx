4331c3d5b547f525cab6e27eaea43a14d5fcfbb9
israrahmedjan <123giIsrar456>
2024-12-13 18:04:23 +0500

    About and contact image added

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -6,47 +6,52 @@ function page() {
     <>
     <div className='mx-20 my-6'>
         <h1 className='text-[25px] font-semibold'>Contact Us</h1>
-        <div className='flex content-center gap-5 my-6'>
-            <div>
-                <Image src="https://images.pexels.com/photos/3868552/pexels-photo-3868552.jpeg?auto=compress&cs=tinysrgb&w=600" 
-                layout="intrinsic"
-                width={1000}
-                height={220}
-                alt='dummy images' />
-            </div>
-            <div>It is a long established fact that a reader will be distracted by 
-                the readable content of a page when looking at its layout. The point 
-                of using Lorem Ipsum is that it has a more-or-less normal distribution 
-                of letters, as opposed to using 'Content here, content here', making it 
-                look like readable English. Many desktop publishing packages and web page 
-                editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' 
-                will uncover many web sites still in their infancy. Various versions have evolved over
-                4 the years, sometimes by accident, sometimes on purpose (injected humour and the like).</div>
+        <div className="flex flex-wrap content-end gap-6 my-6">
+    <div className="md:w-[47%]">
+        <Image 
+            src={`${process.env.NEXT_PUBLIC_DOMAIN}/assets/images/contact2.jpg`}
+            layout="intrinsic"
+            width={1000}
+            height={220}
+            alt="dummy images"
+        />
+    </div>
+    <div className="md:w-[50%]">
+        <h1 className='text-[20px] font-semibold'>Why do we use it? </h1>
+        It is a long established fact that a reader will be distracted by 
+        the readable content of a page when looking at its layout. The point 
+        of using Lorem Ipsum is that it has a more-or-less normal distribution 
+        of letters, as opposed to using 'Content here, content here', making it 
+        look like readable English. Many desktop publishing packages and web page 
+        editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' 
+        will uncover many web sites still in their infancy. Various versions have evolved over
+        the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+    </div>
+</div>
 
-                
-
-        </div>
-
-        <div className='flex content-center gap-5 my-6'>
-            
-            <div>It is a long established fact that a reader will be distracted by 
-                the readable content of a page when looking at its layout. The point 
-                of using Lorem Ipsum is that it has a more-or-less normal distribution 
-                of letters, as opposed to using 'Content here, content here', making it 
-                look like readable English. Many desktop publishing packages and web page 
-                editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' 
-                will uncover many web sites still in their infancy. Various versions have evolved over
-                4 the years, sometimes by accident, sometimes on purpose (injected humour and the like).</div>
-
-                <div>
-                <Image src="https://images.pexels.com/photos/3868552/pexels-photo-3868552.jpeg?auto=compress&cs=tinysrgb&w=600" 
-                layout="intrinsic"
-                width={1000}
-                height={220}
-                alt='dummy images' />
-            </div>
-
-        </div>
+<div className="flex flex-wrap content-end gap-6 my-6">
+   
+    <div className="md:w-[50%]">
+        <h1 className='text-[20px] font-semibold'>Where does it come from? </h1>
+        It is a long established fact that a reader will be distracted by 
+        the readable content of a page when looking at its layout. The point 
+        of using Lorem Ipsum is that it has a more-or-less normal distribution 
+        of letters, as opposed to using 'Content here, content here', making it 
+        look like readable English. Many desktop publishing packages and web page 
+        editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' 
+        will uncover many web sites still in their infancy. Various versions have evolved over
+        the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+    </div>
+    <div className="md:w-[47%]">
+        <Image 
+            src={`${process.env.NEXT_PUBLIC_DOMAIN}/assets/images/contact1.jpeg`}
+            layout="intrinsic"
+            width={1000}
+            height={220}
+            alt="dummy images"
+        />
+    </div>
+</div>
     </div>
     </>
   )
